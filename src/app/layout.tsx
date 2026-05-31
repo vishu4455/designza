540d50c8550a1d4890b61add046278c153ed05ef
Vishal Karthikeya <vishalkarthikeya0704@gmail.com>
2026-05-31 17:40:43 +0530

    designza

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Canva",
+  title: "designza",
   description: "Build Something Great!",
 };
 
